b23fc22d5290a26a167555302f11153c869c9153
baglu <abdullahmshafiq9@gmail.com>
2025-05-05 05:14:38 +0500

    Update app.py with 10000

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def analyze():
 
 import os
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # Use Render's PORT or default to 5000
+    port = int(os.environ.get('PORT', 10000))  # Use Render's PORT or default to 5000
     app.run(host='0.0.0.0', port=port)
 
 
